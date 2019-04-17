257dcd1d63305ce1259e61f4721d665fe3a96a30
MinhQuang <minhquang2971999@gmail.com>
2019-04-17 17:34:43 +0700

    ...

--- 1.js
+++ 1.js
@@ -1,4 +1,7 @@
-﻿ $(function(){
- 
-})  
- 
+﻿$(function () {
+    vitricuaedu = $('#khoiedu').offset().top;
+    console.log(vitricuaedu);
+    $(".xuong,.kn").click(function () {
+        $('body,html').animate({ scrollTop: vitricuaedu }, 600)
+    })
+})
