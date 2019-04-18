a341ec4765981bb304a1433422ecfcf1523d124a
Minh Quang <minhquang2971999@gmail.com>
2019-04-18 13:48:03 +0700

    xu ly fb2

--- 1.js
+++ 1.js
@@ -1,7 +1,19 @@
 ﻿$(function () {
-    vitricuaedu = $('#khoiedu').offset().top;
-    console.log(vitricuaedu);
+
     $(".xuong,.kn").click(function () {
-        $('body,html').animate({ scrollTop: vitricuaedu }, 600)
+        $('body,html').animate({ scrollTop: $('#khoiedu').offset().top }, 600);
+        return false;
     })
+
+    $(".sp").click(function () {
+        $('body,html').animate({ scrollTop: $('#khoisanpham').offset().top }, 600);
+        return false;
+    })
+
+    $(".cm").click(function () {
+        $('body,html').animate({ scrollTop: $('#phanhoi').offset().top }, 600);
+        return false;
+    })
+
+
 })
